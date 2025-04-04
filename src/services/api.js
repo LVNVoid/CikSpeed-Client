35b062ef7956aabb398c56af07790c6b79633ed1
OjekPantai <elvien.purnawan01@gmail.com>
2025-04-05 03:44:41 +0700

    Remove HTTPS agent configuration from API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
-import https from "https";
 
 const api = axios.create({
   baseURL: "https://18.142.54.194:5000/api",
   withCredentials: true,
-  httpsAgent: new https.Agent({ rejectUnauthorized: false }),
 });
 
 export default api;
