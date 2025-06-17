a55db30d321126cf14837578bed56e9f785377d6
OjekPantai <elvien.purnawan01@gmail.com>
2025-06-17 17:35:29 +0700

    Update API base URL to use environment variable for flexibility

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://implemented-occurring-eng-plant.trycloudflare.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
